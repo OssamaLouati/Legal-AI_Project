6af82427ad37ff935e0f3174cdbaa566d6014b4c
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2023-05-18 18:29:24 +0100

    initialise next

---
+++ web_app/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
