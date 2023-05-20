c6df991558a4caf79e272b8159a63b4366f46499
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2023-05-20 16:31:36 +0100

    finished the features section

--- web_app/components/projects.tsx
+++ web_app/components/projects.tsx
@@ -43,15 +43,15 @@ export const Projects = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="dev">
       <Container>
         <Row>
           <Col sm={12}>
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h2>Who we are ?</h2>
+                <p>Our projects have been brought to life with the assistance of a diverse group of skilled INPT students. These talented individuals have contributed their expertise, passion, and dedication to crafting innovative solutions that meet the needs of our clients and users.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -79,7 +79,7 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
+                    <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
--- web_app/components/skills.tsx
+++ web_app/components/skills.tsx
@@ -6,7 +6,11 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 
 export const Skills: React.FC = () => {
   const responsive = {
@@ -27,42 +31,70 @@ export const Skills: React.FC = () => {
       items: 1
     }
   };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '60px',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     // Perform any necessary side effects here
   }, []);
 
   return (
-    <section className="skill" id="skills">
+    <section className="skill" id="features">
       <div className="container">
         <div className="row">
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
-              <h2>Skills</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br /> Lorem Ipsum has been the industry's standard dummy text.</p>
-              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+              <h2>Features</h2>
+              <p>Our plateform offers variety of fabulous features.</p>
+              <Slider {...settings} className="owl-carousel owl-theme skill-slider">
                 <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
+                  <img src="https://i.postimg.cc/k41pQ35K/Untitled-design-1.png" alt="Image" />
+                  <h5>Automated Legal Document Analysis</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="Image" />
-                  <h5>Brand Identity</h5>
+                  <img src="https://i.postimg.cc/43F6wJzf/Untitled-design.png" alt="Image" />
+                  <h5>Ask your contract yourself</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="Image" />
-                  <h5>Logo Design</h5>
+                  <img src="https://i.postimg.cc/Gp5qSPks/Untitled-design-3.png" alt="Image" />
+                  <h5>Identification of Risks and Opportunities</h5>
                 </div>
                 <div className="item">
-                  <img src={meter1} alt="Image" />
-                  <h5>Web Development</h5>
+                  <img src="https://i.postimg.cc/J0tYz6Mx/Untitled-design-2.png" alt="Image" />
+                  <h5>Enhanced Speed and Accuracy</h5>
                 </div>
-              </Carousel>
+              </Slider>
             </div>
           </div>
         </div>
       </div>
-      <img src={colorSharp} className="background-image-left"  alt="Image" />
+      <img src="https://i.postimg.cc/1Xz09km7/Untitled-design-3.png" className="background-image-left"  alt="Image" />
     </section>
   );
 };
