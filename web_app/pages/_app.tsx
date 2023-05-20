da9cf1dd3eac912c2e9d318794e7ea993ada4340
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-05-20 18:09:39 +0100

    finoshed dev sction

--- web_app/pages/_app.tsx
+++ web_app/pages/_app.tsx
@@ -3,6 +3,7 @@ import "./styles.css"
 import "../styles/demo.css"
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
+import Head from "next/head"
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -12,6 +13,11 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Legal Ai</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Add other meta tags, stylesheets, etc. as needed */}
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
