f73adfcbdda4e40346630eb36a4e16e36f9cf28e
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-05-20 16:31:36 +0100

    finished the features section

--- web_app/components/navbar.tsx
+++ web_app/components/navbar.tsx
@@ -41,8 +41,8 @@ export const NavBar = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Features</Nav.Link>
-              <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Get Help</Nav.Link>
+              <Nav.Link href="#features" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Features</Nav.Link>
+              <Nav.Link href="#dev" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Developers</Nav.Link>
             </Nav>
             <span className="navbar-text">
               {/* <div className="social-icon">
