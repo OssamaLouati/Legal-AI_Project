0266511a9b2f8938ddf4e5b41eae7654f2aadb74
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-05-21 16:08:53 +0100

    public folder now working

--- web_app/pages/_app.tsx
+++ web_app/pages/_app.tsx
@@ -5,8 +5,6 @@ import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
 import Head from "next/head"
 
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
