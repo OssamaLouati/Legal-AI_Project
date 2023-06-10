10042657ed5d0ead584ce1cdadcf0d4a555e3480
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2023-06-11 00:44:29 +0100

    added docker files

--- web_app/components/mailchimpform.tsx
+++ web_app/components/mailchimpform.tsx
@@ -3,8 +3,8 @@ import { Newsletter } from "./newsletter";
 
 interface FormattedFormData {
   [key: string]: string;
+  EMAIL: string;
 }
-
 export const MailchimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
 
@@ -12,13 +12,18 @@ export const MailchimpForm = () => {
     <>
       <MailchimpSubscribe
         url={postUrl}
-        render={({ subscribe, status, message }) => (
-          <Newsletter
-            status={status}
-            message={message}
-            onValidated={(formData: FormattedFormData) => subscribe(formData)}
-          />
-        )}
+        render={({ subscribe, status, message }) => {
+          const formattedStatus = status || "";
+          const formattedMessage = message instanceof Error ? "Error occurred" : message || "";
+
+          return (
+            <Newsletter
+              status={formattedStatus}
+              message={formattedMessage}
+              onValidated={(formData: FormattedFormData) => subscribe(formData)}
+            />
+          );
+        }}
       />
     </>
   );
